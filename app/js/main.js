36cddd09900f57702d0d5ca7656f93254bd245d9
FlowerCheng123 <cheng_cong123@163.com>
2016-05-20 16:58:46 +0800

    update

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,9 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 var Hello = require('../component/Hello');
 
 // React.render(<Hello name="Nate" />, document.getElementById('box'));
-React.render(
+ReactDOM.render(
   <h1>Hello, world!</h1>,
   document.getElementById('box')
 );
